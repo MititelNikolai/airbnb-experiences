6b5e8df2f49ab2c6e5f4303dc21274bfe628cd2c
MititelNikolai <mititel.kolya@gmail.com>
2022-08-05 21:16:04 +0300

    Added a crutch for the correct display of images on GitHub pages

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './card.css'
 import STAR from '../../assets/star.svg'
+import katie from '../../assets/img/katie-zaferes.png'
+import mountain from '../../assets/img/mountain-bike.png'
+import wedding from '../../assets/img/wedding-photography.png'
 
 const Card = props => {
 	let badgeText
@@ -9,10 +12,23 @@ const Card = props => {
 	} else if (props.location === 'Online') {
 		badgeText = 'ONLINE'
 	}
+	//This function is necessary for the correct display of images on GitHub Pages
+	function selectImg() {
+		switch (props.coverImg) {
+			case 'katie-zaferes.png':
+				return katie
+			case 'mountain-bike.png':
+				return mountain
+			case 'wedding-photography.png':
+				return wedding
+			default:
+				return 'Image'
+		}
+	}
 	return (
 		<div className='card'>
 			{badgeText && <div className='card__badge'>{badgeText}</div>}
-			<img src={`../../img/${props.coverImg}`} alt='' className='card__image' />
+			<img src={selectImg()} alt='' className='card__image' />
 			<div className='card__ratting'>
 				<img src={STAR} alt='Star' className='card__star' />
 				{props.stats.rating}{' '}
